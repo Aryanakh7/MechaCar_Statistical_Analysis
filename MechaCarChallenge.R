f17932631f5722db655b12fedbe84c3ecc75919b
Aryanakh7 <Aryanakh7@users.noreply.github.com>
2021-04-11 18:35:18 -0400

    Adding Files to the Repository

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,8 @@ MechaCar_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data = MechaCar_data)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data = MechaCar_data))
 
+## Summary Statistics on Suspension Coils
+
+Suspensions_Data<- read.csv(file='suspension_coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- Suspensions_Data %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary <- Suspensions_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
